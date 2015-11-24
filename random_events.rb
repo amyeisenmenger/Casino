d272c90c4521c7057485ebf3f79bc8a1f44e30c3
Amy Eisenmenger <amy.eisenmenger@gmail.com>
2015-11-23 21:19:07 -0700

    intial commit

--- random_events.rb
+++ random_events.rb
@@ -1,9 +1,10 @@
 module Random_Events
-random_events = [ Proc.new(player) { mugged_in_toilet(player) }, ]
-  def mugged_in_toilet(player)
 
+random_events = [ Proc.new(player) { mugged_in_toilet }, ]
+  def mugged_in_toilet(player)
+    puts "#{player.name} worked"
   end
 
 end
 
-#random_events.sample.call(player)
+random_events.sample.call(Player.new("james" ,21 ))
